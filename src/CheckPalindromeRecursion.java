da37615580199b14f6ab00f72a1605b7ff0fb5c0
MuzammilMulla <muzammilmulla999@gmail.com>
2022-08-31 12:40:52 +0530

    Check if String is Palindrome using recursion

---
+++ src/CheckPalindromeRecursion.java
@@ -0,0 +1,22 @@
+public class CheckPalindromeRecursion {
+    public static void main(String[] args) {
+        String str = "-22-";
+        System.out.println(isPalindrome(str,0,str.length()-1));
+    }
+    
+    static boolean isPalindrome(String str, int i, int j){
+        
+        // base condition
+        if(i > j || i==j){
+            return true;
+        }
+
+
+        if(str.charAt(i) != str.charAt(j)){
+            return false;
+        }
+        else{
+           return isPalindrome(str,i+1,j-1);
+        }
+    }
+}
