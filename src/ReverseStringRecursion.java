eec772ebb1fcfaa7d3bef2eacc488709be2bfc9a
MuzammilMulla <muzammilmulla999@gmail.com>
2022-08-31 12:16:37 +0530

    Reversing String with recursion using java

--- src/ReverseStringRecursion.java
+++ src/ReverseStringRecursion.java
@@ -1,8 +1,20 @@
 public class ReverseStringRecursion {
     public static void main(String[] args) {
+        String str= "muzamil";
+        System.out.println(reverse(str));
+    }
+     public static String reverse(String str){
 
+        //base condition
+       if(str.isEmpty()){
+           return str;
+        }
+      return reverse(str.substring(1))+ str.charAt(0);
     }
 }
+
+
+
 
 
 
