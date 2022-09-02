3a75b2f6ea44c54cd0994b50b5a3f2a9225a69f0
Muzamil N Mulla <muzammilmulla999@gmail.com>
2022-09-02 12:58:43 +0530

    Add files via upload

---
+++ src/LengthOfStringRecursion.java
@@ -0,0 +1,17 @@
+public class LengthOfStringRecursion {
+    public static void main(String[] args) {
+        String str = "muza";
+        System.out.println(lengthOfStringRecursion(str, 0,0));
+
+    }
+    static int lengthOfStringRecursion(String str, int i, int count){
+        if(str.length() ==0){
+           // System.out.println("0");
+            return 0;
+        }
+        if(i == str.length()){
+            return count;
+        }
+        return lengthOfStringRecursion(str,i+1,count+1);
+    }
+}
